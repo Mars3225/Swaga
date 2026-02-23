928ec63c5bf7809e630316b5e10082d177673118
Mars3225 <cr0ssfire6598@gmail.com>
2026-02-23 20:42:17 +0500

    minor fixes

--- src/smartDevices/Devices/SmartAirConditioner.kt
+++ src/smartDevices/Devices/SmartAirConditioner.kt
@@ -25,9 +25,4 @@ class SmartAirConditioner(
         isOn = false
         println("Кажись лето закончилось")
     }
-}public class RgbLamp(
-        name: java.lang.String,
-        isOn: Boolean = false,
-        brightness: Int = 50,
-        var color: String = "White"
-)
+}
